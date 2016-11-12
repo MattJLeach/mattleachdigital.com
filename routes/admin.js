540a13dde01a3444312ac89947299187965f9d6f
Matt Leach <email@mattleach.info>
2016-11-12 01:11:37 +0000

    More work on the blog

--- routes/admin.js
+++ routes/admin.js
@@ -10,14 +10,26 @@ router.get('/', function(req, res) {
 });
 
 router.get('/posts', function(req, res) {
-	res.render('admin/posts', {
-  	title: 'Posts'
-  });
+	Post.find({}, function(err, posts) {
+		res.render('admin/posts', {
+	  	title: 'Posts',
+	  	posts: posts
+	  });
+	});
 });
 
 router.get('/posts/add', function(req, res) {
 	res.render('admin/posts-add', {
 		title: 'Add Post'
+	});
+});
+
+router.post('/posts/add', function(req, res) {
+	Post.create(req.body.post, function(err, post) {
+		if (err) {
+			console.error(err);
+		}
+		res.redirect('/admin/posts');
 	});
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var Post = require('../models/post');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
   	title: 'Matt Leach',
   	subTitle: 'Musings of a Javascript developer'
   });
+});
+
+router.get('/blog', function(req, res) {
+	Post.find({}, function(err, posts) {
+		if (err) {
+			console.error(err);
+		}
+		res.render('blog', {
+			title: 'Blog',
+			posts: posts
+		});
+	});
 });
 
 module.exports = router;
