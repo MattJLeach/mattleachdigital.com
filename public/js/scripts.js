bac206ff54e9c8614ac656755c4c35b1db8d9383
Matt Leach <email@mattleach.info>
2016-11-16 01:21:42 +0000

    nav bugfix

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,12 @@
 function toggleNav() {
 	var x = document.getElementById('site-nav');
+	var b = document.getElementById('navButton');
+
 	if (x.className === 'site-nav') {
 		x.className += ' open';
+		b.className = 'fa fa-times fa-2x'
 	} else {
 		x.className = 'site-nav';
+		b.className = 'fa fa-bars fa-2x'
 	}
 }
